b9516829b11fd13f7386ab4b30cb0861e4f2fa1d
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-15 17:16:08 -0500

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,4 +12,15 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(100),
 salary DECIMAL(10,2),
 department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT NOT NULL
 );
