e7ce702454198d29242263ea98fa131c0bb67860
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-15 16:31:13 -0500

    connection

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db",
+    });
