9854b0557d159a7aba068c96fa3f49b5ab3ccc6d
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-15 16:37:22 -0500

    departments

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,7 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(255) NOT NULL
+);
