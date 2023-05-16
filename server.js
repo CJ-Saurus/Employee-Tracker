b9db5fe3186a6f35d3a9beb7bfd576f45beae31f
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-15 22:01:39 -0500

    update

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-const connection = require('./db/connection.js');
-const PORT = process.env.PORT || 3306;
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeeTracker_db",
+});
+
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to the database!");
-  start();
+    if (err) throw err;
+    console.log("Connected to the database!");
+    start();
 });
 
 function start() {
