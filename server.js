298033324e22d317b77d7c3a180cf7f346a658e0
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-15 21:10:45 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-const connection = require("./db/connection") ({
+const connection = mysql.createConnection ({
   host: "localhost",
   port: 3306,
   user: "root",
